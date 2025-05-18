3f6a593ba90c5f5e49ad4cb4db0809ca3403f79b
tangshulin <2962380651@qq.com>
2025-05-19 01:26:59 +0800

    02.登录注册表单

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,108 @@
 <script setup>
-
+  import { ref } from 'vue'
+//控制注册和登录的表单
+  const isRegister = ref(true)
 </script>
 
 <template>
-  <div>
-    x
-  </div>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <!-- 注册相关表单 -->
+      <el-form v-if="isRegister">
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入再次密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+
+      <!-- 登录相关表单 -->
+      <el-form size="large" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>登录</el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.login-page {
+  height: 98.3vh;
+  margin: 0;
+  background-color: #fff;
 
+  .el-row {
+      height: 100%;
+    }
+
+  .bg {
+    height: 100%;
+    background:url('@/assets/login/bg.png') no-repeat center / cover;
+    border-radius: 10px;
+    border: 0px solid white;
+    box-sizing: border-box;
+    overflow: hidden;
+  }
+
+  .form {
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
 </style>
